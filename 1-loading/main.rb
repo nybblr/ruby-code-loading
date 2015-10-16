8d36261e5e32e5b8ef7413d39315b535fafb7a00
Jonathan Martin <me@nybblr.com>
2015-10-16 16:51:51 -0400

    Add output, fix eval example.

--- 1-loading/main.rb
+++ 1-loading/main.rb
@@ -1,4 +1,5 @@
 eval "puts to_s"
+# => main
 
 # ---------------------
 
@@ -9,27 +10,35 @@ class Cook
 end
 
 eval "puts to_s", Cook.new.get_binding
+# => #<Cook:0x007f88918e3110>
 
 # ---------------------
 
 dir  = File.expand_path("..", __FILE__)
 file = File.join(dir, "cookery/recipe.rb")
 
-eval file, binding, "cookery/recipe.rb", 1
+eval File.read(file), binding, "cookery/recipe.rb", 1
+# => Loaded Cookery::Recipe class!
 
 # ---------------------
 
 load File.join(dir, "cookery/recipe.rb")
+# => Loaded Cookery::Recipe class!
 
 # ---------------------
 
 $LOAD_PATH.unshift(dir)
 
 load "cookery/recipe.rb"
+# => Loaded Cookery::Recipe class!
 
 # ---------------------
 
 require File.join(dir, "cookery/recipe.rb")
+# => Loaded Cookery::Recipe class!
+
 require "cookery/recipe"
+# Eerie silence...
 
 p $LOADED_FEATURES.last
+# => "/home/nybblr/cookery/recipe.rb"
