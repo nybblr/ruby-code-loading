1a8f885b01abf1068c850d40ac9f48da80be1681
Jonathan Martin <me@nybblr.com>
2015-10-31 13:54:16 -0400

    Finish out 6-eagerload examples.

--- 6-eagerload/application.rb
+++ 6-eagerload/application.rb
@@ -1,6 +1,9 @@
 module Cookery
   class Application < Rails::Application
-    # config.autoload_paths += %W(#{config.root}/lib)
-    config.eager_load_paths += %W(#{config.root}/lib)
+    config.autoload_paths << "#{config.root}/lib"
+    config.eager_load_paths << "#{config.root}/lib"
+    config.eager_load_paths +=
+      ["#{config.root}/app/models",
+       "#{config.root}/app/models/queries"]
   end
 end
