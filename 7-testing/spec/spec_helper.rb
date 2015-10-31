217e76a387a0c89e6b370c7b1838096992ffeb86
Jonathan Martin <me@nybblr.com>
2015-10-31 14:15:38 -0400

    Reorder 7-testing blocks.

--- 7-testing/spec/spec_helper.rb
+++ 7-testing/spec/spec_helper.rb
@@ -1,13 +1,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 
-# General config
-RSpec.configure do |config|
-  config.order = :random
-end
-
 # Stub out common Rails methods
 if !Object.const_defined?(:Rails)
-
   require 'active_support/all'
 
   Rails = Module.new do
@@ -22,4 +16,9 @@ if !Object.const_defined?(:Rails)
 
   ActiveSupport::Dependencies.autoload_paths +=
     Dir["#{Rails.root}/app/*/"]
+end
+
+# General config
+RSpec.configure do |config|
+  config.order = :random
 end
