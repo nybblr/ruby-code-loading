ec7fa41c95681f1ee51d2839b34e6e7da1056c5a
Jonathan Martin <me@nybblr.com>
2015-10-31 13:35:39 -0400

    Step-by-step 4-constants walkthrough.

--- 4-constants/cookery/recipe.rb
+++ 4-constants/cookery/recipe.rb
@@ -3,6 +3,5 @@ puts "Loaded Cookery::Recipe class!"
 module Cookery
   class Recipe
     CONSTANT = "constant".freeze
-    # CONSTANT ||= "constant".freeze
   end
 end
--- 4-constants/main.rb
+++ 4-constants/main.rb
@@ -4,9 +4,35 @@ ROOT = File.expand_path("..", __FILE__)
 
 $LOAD_PATH.unshift(ROOT)
 
+# ActiveSupport::Dependencies.mechanism = :load
 ActiveSupport::Dependencies.autoload_paths << ROOT
 
+p $LOADED_FEATURES.last
+# => ".../active_support/dependencies.rb"
 Cookery::Recipe
+# => Loaded Cookery::Recipe class!
+p $LOADED_FEATURES.last
+# => ".../active_support/dependencies.rb"
 require 'cookery/recipe'
-
+# => Loaded Cookery::Recipe class!
 # warning: already initialized constant Cookery::Recipe::CONSTANT
+# warning: previous definition of CONSTANT was here
+
+# Constant redef warnings. Avoided if NO_RELOAD was set (production).
+
+# ---------------------
+
+ActiveSupport::Dependencies.mechanism = :require
+ActiveSupport::Dependencies.autoload_paths << ROOT
+
+p $LOADED_FEATURES.last
+# => ".../active_support/dependencies.rb"
+Cookery::Recipe
+# => Loaded Cookery::Recipe class!
+p $LOADED_FEATURES.last
+# => "/home/nybblr/cookery.rb"
+require 'cookery/recipe'
+# *Eerie silence...*
+
+# Fixed by using require, but now we can't reload in dev!
+# Lesson learned: Just don't use require. Stick with autoloading, mix and match at your risk. It will find your intern.
